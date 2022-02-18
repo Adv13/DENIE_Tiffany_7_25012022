905f9dd533d887c15e5f389877286651e8ff01a8
Adv <tiffanydpro923@gmail.com>
2022-02-18 12:42:01 +0100

    maj css file

--- assets/js/API/recipesData.js
+++ assets/js/API/recipesData.js
@@ -961,7 +961,7 @@ const recipesApiResult = [{
             }
         ],
         "time": 80,
-        "description": "Emincer les blanc de poireaux et les faire chauffer dans 25 grammes de beurre. AJouter les pommes de terres coupées en morceaux. Ajouter l'eau et laisser mijoter pour 45 minutes. Chauffer l'oseille avec le beurre restant puis incorporer le tout. Mixez. Ajoutez la crème. Bon appetit.",
+        "description": "Emincer les blanc de poireaux et les faire chauffer dans 25 grammes de beurre. Ajouter les pommes de terres coupées en morceaux. Ajouter l'eau et laisser mijoter pour 45 minutes. Chauffer l'oseille avec le beurre restant puis incorporer le tout. Mixez. Ajoutez la crème. Bon appetit.",
         "appliance": "Mixer",
         "ustensils": ["casserolle", "couteau"]
     },
